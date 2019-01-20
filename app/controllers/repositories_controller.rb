0b5f78c59844e516ae26390da6decef7f5b37fbd
Hillevi Eklöw <hillevieklow@gmail.com>
2019-01-20 19:15:46 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -12,6 +12,6 @@ class RepositoriesController < ApplicationController
     end
     body = JSON.parse(@resp.body)
     @results = body["items"]
-    render 'app/views/repositories/search'
+    render 'search'
   end
 end
